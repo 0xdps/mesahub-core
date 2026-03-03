8f910ffe423c7e2f7fe248a91990eae1b6326ded
Devendra Pratap Singh <dps.manir@gmail.com>
2026-03-03 06:34:43 +0530

    Revert "chore: upgrade TypeScript to 6.0.0-beta, add CSS ambient declaration"
    This reverts commit 9d9f4186924e82fe15435ee70acc40c9f0600cac.

--- src/css.d.ts
+++
@@ -1,5 +0,0 @@
-// Ambient declaration for CSS module/side-effect imports (required by TypeScript 6)
-declare module "*.css" {
-  const content: Record<string, string>;
-  export default content;
-}
