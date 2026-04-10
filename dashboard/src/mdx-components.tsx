39ec6ad08d10e7ffcdcf990fdfa328382be0e31a
Devendra Pratap Singh <dps.manit@gmail.com>
2026-04-10 14:49:05 +0000

    refactor: simplify useMDXComponents function by removing custom component handling

--- dashboard/src/mdx-components.tsx
+++ dashboard/src/mdx-components.tsx
@@ -1,12 +1,3 @@
-import type { MDXComponents } from "mdx/types";
-import CodeBlock from "./components/code-block";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-    code: (props) => {
-      return <code {...props} />;
-    },
-    pre: CodeBlock,
-  };
+export function useMDXComponents(components: Record<string, unknown>): Record<string, unknown> {
+  return { ...components }
 }
